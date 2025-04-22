45121df30b2d15559a153922844661dfb09deaf5
IvanNugiban <aa1245541@gmail.com>
2025-04-22 20:51:22 +0200

    Added telegram bot integration and filtering skins by criterias, etc.

---
+++ misc/telegram_bot.py
@@ -0,0 +1,12 @@
+import telegram
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
